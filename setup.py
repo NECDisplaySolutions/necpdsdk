ceca87837d4979fdef6eaf9bba5e03f74fbf3e9e
necdisplay <tmarr@necdisplay.com>
2017-03-16 15:14:17 -0500

    Commit the correct setup.py and changelog

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0a1',
+    version='1.0.1a1',
 
     description='NEC PD SDK',
     long_description=long_description,
